9dfcbc8debf63d1ea3b9ee0f73abc775d341c316
Lucas Signorelli <signorellilucas7@gmail.com>
2020-06-28 17:41:48 +0200

    Adding the celsius

--- static/app.js
+++ static/app.js
@@ -6,16 +6,18 @@ window.addEventListener("load", ()=> {
 
     let degree = document.querySelector('.degree');
     let timezone = document.querySelector('.timezone');
-    let temperature = document.querySelector('.temperature');
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector('.temperature span')
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
+    
             lat = position.coords.latitude;
             lon = position.coords.longitude;
-
             const proxy = "https://cors-anywhere.herokuapp.com/";
             const api = `${proxy}https://api.darksky.net/forecast/edbea218432ac27de1b3da161f4c60eb/${lat},${lon}`;
-            
+
+                        
             fetch(api)
                 .then(response => {
                     return response.json();
@@ -28,14 +30,25 @@ window.addEventListener("load", ()=> {
                     degree.textContent = temperature;
                     timezone.textContent = data.timezone;
 
+                    let celsius = (temperature - 32) * (5 / 9);
+
                     setIcons(icon, document.querySelector('.icon'));
 
+                    temperatureSection.addEventListener("click", ()=> {
+                        if (temperatureSpan.textContent === "F") {
+                            temperatureSpan.textContent === "C";
+                            degree.textContent = Math.floor(celsius);
+                        } else {
+                            temperatureSpan.textContent === "F";
+                            degree.textContent = temperature
+                        }
+                    });
                 });
         });
     }
 
     function setIcons(icon, iconID) {
-        const skycons = new Skycons({color : "black"});
+        const skycons = new Skycons({color : "white"});
         const currentIcon = icon.replace(/-/g, "_").toUpperCase();
         skycons.play();
         return skycons.set(iconID, Skycons[currentIcon]);
