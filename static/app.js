78d15955ee6fcc8a8367360e94b55f6591281378
Lucas Signorelli <signorellilucas7@gmail.com>
2020-06-29 18:55:23 +0200

    Adding icon and fix fahrenheit bug

--- static/app.js
+++ static/app.js
@@ -2,12 +2,11 @@ window.addEventListener("load", ()=> {
 
     let lat;
     let lon;
-
-
     let degree = document.querySelector('.degree');
     let timezone = document.querySelector('.timezone');
     let temperatureSection = document.querySelector('.temperature');
-    const temperatureSpan = document.querySelector('.degree-section span')
+
+    const temperatureSpan = document.querySelector('.degree-section span');
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -15,7 +14,7 @@ window.addEventListener("load", ()=> {
             lat = position.coords.latitude;
             lon = position.coords.longitude;
             const proxy = "https://cors-anywhere.herokuapp.com/";
-            const api = `${proxy}https://api.darksky.net/forecast/edbea218432ac27de1b3da161f4c60eb/${lat},${lon}`;
+            // const api = `${proxy}https://api.darksky.net/forecast/edbea218432ac27de1b3da161f4c60eb/${lat},${lon}`;
 
                         
             fetch(api)
@@ -26,11 +25,11 @@ window.addEventListener("load", ()=> {
                     console.log(data);
 
                     const {temperature, icon} = data.currently;
-
                     degree.textContent = temperature;
                     timezone.textContent = data.timezone;
 
                     let celsius = (temperature - 32) * (5 / 9);
+                    let fahrenheit = temperature
 
                     setIcons(icon, document.querySelector('.icon'));
 
@@ -41,7 +40,7 @@ window.addEventListener("load", ()=> {
                         } else {
                             if (temperatureSpan.textContent === "C") {
                                 temperatureSpan.textContent = "F";
-                                degree.textContent = degree;
+                                degree.textContent = temperature;
                             }
                         }
                         
